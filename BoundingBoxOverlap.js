1dfcba173ba1537e2e3082675d630062c2e940a0
ma-hei <marten_heidemeyer@web.de>
2014-08-05 11:48:38 +0200

    refactorization and improvement of collision detection

--- BoundingBoxOverlap.js
+++ BoundingBoxOverlap.js
@@ -9,3 +9,27 @@ function Overlap(){
     
     
 }
+
+function BoundingBox(){
+    
+    this.bodya = -1;
+    this.bodyb = -1;
+    this.bodyWithSeparatingEdge = -1;
+    this.separatingEdge = -1;
+    this.collision = -1;
+    this.interpenetratingVertexOfOtherBody = -1;
+    
+    
+}
+
+function InformationOfSeparation(){
+    
+    this.bodya = -1;
+    this.bodyb = -1;
+    this.bodyWithSeparatingEdge = -1;
+    this.separatingEdge = -1;
+    this.collision = -1;
+    this.interpenetratingVertexOfOtherBody = -1;
+    
+    
+}
